1a0d49e0931750c20606e7a631c801e62fd8a0bf
haaslaura <110221401+haaslaura@users.noreply.github.com>
2024-01-08 15:42:39 +0100

    Test to ad ARIA attribute & focus to the modal when open/close

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,56 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+
+    // Passer le reste du site à aria-hidden true
+    const otherContent = document.querySelectorAll("body > *:not(#contact_modal)");
+    otherContent.forEach(element => {
+        element.setAttribute("aria-hidden", "true");
+    });
+
+    // Passer la modale à aria-hidden false
+    modal.setAttribute("aria-hidden", "false");
+
+    modal.style.display = "block";
+
+    // Gestion du focus
+    const currentFocusedElement = document.querySelector("contact_modal");
+    if (!currentFocusedElement) {
+        currentFocusedElement.focus();
+    } // A CORRIGER
+
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
+
+    // Passer le reste du site à aria-hidden false
+    const otherContent = document.querySelectorAll("body > *:not(#contact_modal)");
+    otherContent.forEach(element => {
+        element.setAttribute("aria-hidden", "false");
+    });
+
+    // Passer la modale à aria-hidden true
+    modal.setAttribute("aria-hidden", "true");
+
+    // Gestion du focus
+    const previouslyFocusedElement = document.querySelector("contact_modal");
+    if (previouslyFocusedElement) {
+        previouslyFocusedElement.focus();
+    }
+
+    // Gestion de la fermeture avec la touche echap
+    document.removeEventListener('keydown', handleKeyDown);
+
     modal.style.display = "none";
 }
+
+ 
+// Close modal when escape key is pressed
+ function handleKeyDown(event) {
+    const modal = document.getElementById("contact_modal");
+    if (event.key === "Escape" && modal.style.display !== "none") {
+        closeModal();
+    }
+}
+
+document.addEventListener('keydown', handleKeyDown); // à corriger
