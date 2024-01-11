64e58fc4a35951aad03aaba8b55e464852dbfece
haaslaura <110221401+haaslaura@users.noreply.github.com>
2024-01-11 10:01:56 +0100

    Notes for the photographer page

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,4 +1,43 @@
 // This JavaScript code is linked to the photographer.html page
 
-const testPhotographer = document.querySelector("header");
-console.log(testPhotographer);
+// Récupérer la section photograph-header
+const photographHeader = document.querySelector(".photograph-header");
+console.log(photographHeader);
+
+/*
+Colonne 1 :
+Nom
+Ville, Pays
+Slogan
+
+Colonne suivante :
+Bouton
+
+Colonne 3 :
+Photo
+*/
+
+// 1 - Récupérer les éléments depuis le fichier Json
+
+// 2 - Préparer une boucle for pour générer toutes les images
+
+// 3 - Récupérer l'élément du DOM qui contiendra toutes les photos
+
+// 4 - Créer / ajouter l'élement qui contiendra la photo, le titre et le like (et la date, même si ce n'est pas visible)
+
+// 5 - Ensuite on s'occupe des boutons 
+// Ajouter les boutons ici et ne faire que la fonctionnalité eventListener sur filterButton
+
+
+
+
+
+
+
+
+
+
+// Récupération les infos depuis l'url
+
+const searchParams = new URLSearchParams(window.location.search);
+console.log(searchParams.get('id')); 
