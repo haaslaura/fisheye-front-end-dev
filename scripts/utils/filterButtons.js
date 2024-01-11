9a98a16a935a260bc2ed7ba3e8aa612d66b06470
haaslaura <110221401+haaslaura@users.noreply.github.com>
2024-01-11 10:03:40 +0100

    Notes for the filter buttons fonction

--- scripts/utils/filterButtons.js
+++ scripts/utils/filterButtons.js
@@ -13,17 +13,18 @@ const popularityBtn = document.querySelector(".filter-popularity");
 const dateBtn = document.querySelector(".filter-date");
 const titleBtn = document.querySelector(".filter-title");
 
-console.log(popularityBtn);
-console.log(dateBtn);
-console.log(titleBtn);
+// pour pouvoir utiliser la fonction dans photographer.js, j'utilise 'export'
+export function filter() {
 
-popularityBtn.addEventListener("click", function () {
-    const arrayOrdonne = Array.from(data.media); // (sortir ce bloc) copie du tableau pour conserver inchangé l'original
-    arrayOrdonne.sort(function(nbLikeA, nbLikeB){
-        return nbLikeA.likles - nbLikeB.likes;
+    popularityBtn.addEventListener("click", function () {
+        const arrayOrdonne = Array.from(data.media); // (sortir ce bloc) copie du tableau pour conserver inchangé l'original
+        arrayOrdonne.sort(function(nbLikeA, nbLikeB){
+            return nbLikeA.likles - nbLikeB.likes;
+        });
+        console.log(arrayOrdonne); // vérifier que le tableau est bien ré-ordonné dans la console
     });
-    console.log(arrayOrdonne); // vérifier que le tableau est bien ré-ordonné dans la console
-});
 
+
+}
 /* Transformer les données Json en tableau ? */
 /* nameArray = media ? */
