de24ecbaf797bc123588ec9a206339e5c51077a1
haaslaura <110221401+haaslaura@users.noreply.github.com>
2024-01-18 09:28:22 +0100

    Minor corrections js

--- scripts/utils/filterButtons.js
+++ scripts/utils/filterButtons.js
@@ -23,13 +23,13 @@ const titleBtn = document.querySelector(".filter-title");
 // pour pouvoir utiliser la fonction dans photographer.js, j'utilise 'export'
 function filter() {
 
-    popularityBtn.addEventListener("click", function () {
-        const arrayOrdonne = Array.from(data.media); // (sortir ce bloc) copie du tableau pour conserver inchangé l'original
-        arrayOrdonne.sort(function(nbLikeA, nbLikeB){
-            return nbLikeA.likles - nbLikeB.likes;
-        });
-        console.log(arrayOrdonne); // vérifier que le tableau est bien ré-ordonné dans la console
-    });
+	popularityBtn.addEventListener("click", function () {
+		const arrayOrdonne = Array.from(data.media); // (sortir ce bloc) copie du tableau pour conserver inchangé l'original
+		arrayOrdonne.sort(function(nbLikeA, nbLikeB){
+			return nbLikeA.likles - nbLikeB.likes;
+		});
+		console.log(arrayOrdonne); // vérifier que le tableau est bien ré-ordonné dans la console
+	});
 
 
 }
