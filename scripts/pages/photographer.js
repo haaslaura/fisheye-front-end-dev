397055b4a1913d2de51cfc98ffe1f17c02a72cc0
haaslaura <110221401+haaslaura@users.noreply.github.com>
2024-01-18 09:26:13 +0100

    Correction of the photographerTemplate function for use on the photographer page

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,46 +1,66 @@
 // This JavaScript code is linked to the photographer.html page
 
-// Récupérer la section photograph-header
-const photographHeader = document.querySelector(".photograph-header");
-console.log(photographHeader);
 
-/*
-Colonne 1 :
-Nom
-Ville, Pays
-Slogan
+// 1 - Importer les données (et autres fonctions)
+import { Picture } from '../media/Picture.js';
+import { photographerTemplate } from '../templates/photographerInfo.js';
+import { getData } from '../utils/getData.js';
 
-Colonne suivante :
-Bouton
-
-Colonne 3 :
-Photo
-*/
-
-// 1 - Récupérer les éléments depuis le fichier Json
-
-// 2 - Préparer une boucle for pour générer toutes les images
-
-// 3 - Récupérer l'élément du DOM qui contiendra toutes les photos
-
-// 4 - Créer / ajouter l'élement qui contiendra la photo, le titre et le like (et la date, même si ce n'est pas visible)
-
-// 5 - Ensuite on s'occupe des boutons 
-// Ajouter les boutons ici et ne faire que la fonctionnalité eventListener sur filterButton
+// 2a - Only retrieve photographer from his id (étape 2b en bas de la page)
+async function getPhotographerById(id) {
+    try {
+        // Retrieve data
+        const data = await getData();
+        
+        // Check whether the data contains photographers
+        if (data?.photographers) {
+            const photographersArray = data.photographers;
+            return photographersArray.find((element) => element.id === id);
+        }
+        console.log("Aucune donnée sur le photographe trouvée.");
+    } catch (error) {
+        console.error("Erreur lors de l'affichage du photographe :", error);
+    }
+}
 
 
+// 3- Afficher les info du photographe dont c'est l'id
+
+function displayData(photographer) {
+    
+    // Recovery of the photographers' divide
+    const photographersHeader = document.querySelector(".photograph-header");
+    const headerCol1 = document.getElementById("photograph-header-col1");
+    const headerCol3 = document.getElementById("photograph-header-col3");
+    
+    // Utilise le template
+    const photographerModel = photographerTemplate(photographer);
+    const items = photographerModel.createItems();
+
+    headerCol1.appendChild(items.namePhotographer);
+    headerCol1.appendChild(items.locationText);
+    headerCol1.appendChild(items.sloganText);
+    headerCol3.appendChild(items.imagePhotographer);
+}
+
+
+async function initPage() {
+    // 2b - Récupérer les informations du photographe uniquement avec son id
+    let params = new URLSearchParams(document.location.search);
+    let idItem = parseInt(params.get("id"));
+    console.log(idItem);
+
+    // Recovers data from photographer
+    const photographer = await getPhotographerById(idItem);
+    displayData(photographer);
+}
+
+initPage();
 
 
 
+// 4 - Récupérer les infos média du photographe dont c'est l'id
 
+// 5 - Afficher les médias
 
-
-
-
-// Récupération les infos depuis l'url
-
-/*
-const searchParams = new URLSearchParams(window.location.search);
-console.log(searchParams.get('id')); 
-
-*/
+// 6 - Trier les médias
---
+++ scripts/templates/photographerInfo.js
@@ -0,0 +1,62 @@
+export function photographerTemplate(photographersData) {
+	// Déstructure l'objet photographersData et crée autant de constante que nécessaire
+	const { name, id, portrait, city, country, tagline, price } = photographersData;
+
+	// Crée la constante picture => pour ajouter le chemin du dossier où se trouve les photos au nom de la photo
+	const picture = `assets/photographers/${portrait}`;
+
+	function createItems() {
+		const imagePhotographer = document.createElement("img");
+		imagePhotographer.setAttribute("src", picture);
+		// Alt vide permet d'ignorer la photo pour les lecteurs d'écran
+		imagePhotographer.setAttribute("alt", "");
+
+		const namePhotographer = document.createElement("h2");
+		namePhotographer.textContent = name;
+
+		const locationText = document.createElement("h3");
+		locationText.textContent = `${city}, ${country}`;
+
+		const sloganText = document.createElement("h4");
+		sloganText.textContent = tagline;
+
+		const priceText = document.createElement("p");
+		priceText.textContent = price ? `${price}€/jour` : "Aucune information pour le moment";
+
+		return {
+			imagePhotographer,
+			namePhotographer,
+			locationText,
+			sloganText,
+			priceText,
+		}
+	}
+
+	function createUserCardDOM() {
+		// Creating the 'article' item
+		const articlePhotographer = document.createElement("article");
+
+		// Creating elements to include in 'article'
+		const linkPhotographerPage = document.createElement("a");
+		linkPhotographerPage.setAttribute("href", `/photographer.html?id=${id}`);
+		linkPhotographerPage.setAttribute("role", "button");
+		linkPhotographerPage.setAttribute("aria-label", `Lien vers la page de ${name}`);
+
+		const portraitPhotographer = document.createElement("div");
+
+		const {imagePhotographer, locationText, namePhotographer, priceText, sloganText} = createItems();
+
+		// Integrating elements
+		articlePhotographer.appendChild(linkPhotographerPage);
+		linkPhotographerPage.appendChild(portraitPhotographer);
+		portraitPhotographer.appendChild(imagePhotographer);
+		portraitPhotographer.appendChild(namePhotographer);
+		articlePhotographer.appendChild(locationText);
+		articlePhotographer.appendChild(sloganText);
+		articlePhotographer.appendChild(priceText);
+       
+		return (articlePhotographer);
+	}
+
+	return { createItems, createUserCardDOM };
+}
